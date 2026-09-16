0182517743509b75c5a04f9154a2d63dc4b37387
Jhansirani <jhansi1229@gmail.com>
2026-09-17 00:48:04 +0800

    Create snowpipe_auto_ingest.sql for data ingestion
    Add SQL script for manual and automatic ingestion using Snowpipe.

---
+++ 02_auto_ingestion/snowpipe_auto_ingest.sql
@@ -0,0 +1,35 @@
+-- ============================================================
+-- TEST MANUAL INGESTION
+-- ============================================================
+
+-- First test:
+-- S3 CSV files → Snowflake sales_data1
+COPY INTO sales_data1
+FROM @sales_stg1
+ON_ERROR = CONTINUE;
+
+
+SELECT * FROM sales_data1;
+
+
+-- ============================================================
+-- SNOWPIPE AUTOMATIC INGESTION
+-- ============================================================
+
+-- Snowpipe automatically loads new files
+-- arriving in the S3 location into sales_data1.
+
+CREATE OR REPLACE PIPE sales_datapipe1
+  AUTO_INGEST = TRUE
+AS
+COPY INTO sales_data1
+FROM @sales_stg1
+ON_ERROR = CONTINUE;
+
+
+-- Check Snowpipe configuration.
+SHOW PIPES;
+
+
+-- Check loaded data.
+SELECT * FROM sales_data1;
